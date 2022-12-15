cd84b319b2b2eea26e814e6f14e6e0cbc6488219
Pitri Indrayani <indrayanipitri@gmail.com>
2022-12-15 17:14:39 +0700

    update parent oleh child component part 1

--- src/Component/Statefull/Product/Product.jsx
+++ src/Component/Statefull/Product/Product.jsx
@@ -4,17 +4,39 @@ import Troley from '../../../images/shopping-cart.png'
 import Bubur from '../../../images/bubur.jpeg'
 
 class Product extends Component {
+    state = {
+        order: 4
+    }
+
+   
+    handlePlus = () => {
+       this.setState ({
+        order: this.state.order + 1
+       })
+    }
+
+    handleMinus = () => {
+        if (this.state.order > 0) {
+            this.setState ({
+                order: this.state.order - 1
+            })
+        }
+       
+    }
+
+
     render() {
         return (
             <>
-                <div className="card-wrapper">
+                <div className="header">
                     <div className="logo">
                         <h2>Pipit</h2>
                     </div>
                     <div className="troley">
                         <img src={Troley} alt="" />
-                        <div className="count">3</div>
+                        <div className="count">{this.state.order}</div>
                     </div>
+                </div>
                     <div className="card">
                         <div className="img-thumb-prod">
                             <img src={Bubur} alt="" />
@@ -22,12 +44,11 @@ class Product extends Component {
                         <p className="product-title">Bubur Ayam</p>
                         <p className="product-price">Rp 20.000</p>
                         <div className="counter">
-                            <button className="minus">-</button>
-                            <input type="text" value={3} />
-                            <button className="plus">+</button>
+                            <button className="minus" onClick={this.handleMinus}>-</button>
+                            <input type="text" value={this.state.order} />
+                            <button className="plus" onClick={this.handlePlus}>+</button>
                         </div>
                     </div>
-                </div>
             </>
             
         )
